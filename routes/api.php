85d21029ab6a20849d6f86daec160c622ed761da
Jorge <jorgetoribio.daw@gmail.com>
2026-01-28 20:55:53 +0100

    Cambios ligeros en el router api

--- routes/api.php
+++ routes/api.php
@@ -33,20 +33,26 @@
             }
 
         }
-        // Lista de provincias
-        $provincias = ['León', 'Salamanca', 'Burgos', 'Ávila', 'Soria', 'Segovia', 'Palencia','Valladolid','Zamora'];
-        $allData = [];
-        foreach ($provincias as $provincia){
-            $apiUrl = 'https://analisis.datosabiertos.jcyl.es/api/explore/v2.1/catalog/datasets/registro-de-turismo-de-castilla-y-leon/records?where=provincia%20%3D%20%27' . urldecode($provincia) . '%27&limit=20&refine=establecimiento%3A%22Campings%22';
-            $data = apiSummon($apiUrl);
-            if(isset($data['results'])){
-                $allData = array_merge($allData, $data['results']);
+        if(isset($_SERVER['REQUEST_METHOD'])){
+            // Lista de provincias
+            $provincias = ['León', 'Salamanca', 'Burgos', 'Ávila', 'Soria', 'Segovia', 'Palencia','Valladolid','Zamora'];
+            $allData = [];
+            foreach ($provincias as $provincia){
+                $apiUrl = 'https://analisis.datosabiertos.jcyl.es/api/explore/v2.1/catalog/datasets/registro-de-turismo-de-castilla-y-leon/records?where=provincia%20%3D%20%27' . urldecode($provincia) . '%27&limit=20&refine=establecimiento%3A%22Campings%22';
+                $data = apiSummon($apiUrl);
+                if(isset($data['results'])){
+                    $allData = array_merge($allData, $data['results']);
+                }
+            }
+            //$apiUrlTesting = 'https://analisis.datosabiertos.jcyl.es/api/explore/v2.1/catalog/datasets/registro-de-turismo-de-castilla-y-leon/records?select=*&where=provincia%20%3D%20%27Salamanca%27&limit=100&refine=establecimiento%3A%22Campings%22';
+            //var_dump(apiSummon($apiUrlTesting));
+            json_encode($allData);
+            $apiController = new \Controllers\ApiController();
+            $api_method = $_SERVER['REQUEST_METHOD'];
+            if($api_method === 'GET'){
+                var_dump($allData);
             }
         }
-        //$apiUrlTesting = 'https://analisis.datosabiertos.jcyl.es/api/explore/v2.1/catalog/datasets/registro-de-turismo-de-castilla-y-leon/records?select=*&where=provincia%20%3D%20%27Salamanca%27&limit=100&refine=establecimiento%3A%22Campings%22';
-        //var_dump(apiSummon($apiUrlTesting));
-        json_encode($allData);
-        var_dump($allData);
 
     }catch(Exception $e){
         header('HTTP/1.1 500 Internal Server Error');
